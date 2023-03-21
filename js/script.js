8480ac5b2e4f4676f454e07f804c32aa6478c987
anashamimi <anashamimi2@gmail.com>
2023-03-21 15:42:33 +0100

    add random numbers

--- js/script.js
+++ js/script.js
@@ -1,15 +1,76 @@
+/*
+Descrizione:
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, i numeri che ha visto precedentemente.
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+const btnStart = document.getElementById('start');
+
 const countDown = document.getElementById('countDown');
 
 let count = 5;
 
 countDown.innerHTML = count;
 
-const timer = setInterval (() => {
-    if (count === 0) {
-        clearInterval(timer);
-        count = 'Il tempo è terminato';
+const numbers = document.getElementById('numbers');
+
+
+
+//Funzione per generare l'array dei numeri casuali
+function generateNumbers() {
+    const nums = [];
+    while (nums.length < 5) {
+        const num = getRndNumber(1, 5);
+        if (!nums.includes(num)) {
+            nums.push(num);
+        }
+    }
+    return nums;
+}
+
+//Funzione per generare l'array di numeri casuali
+function generateNums() {
+    const nums = [];
+    while (nums.length < 5) {
+        const num = getRndNumber(1, 99);  
+        if(!nums.includes(num)) {
+            nums.push(num); 
+        }          
+    }
+    return nums;
+}
+
+btnStart.addEventListener('click', play);
+
+function play(e) {
+    e.preventDefault();   
+    
+    nums = generateNums();
+    console.log(nums);
+
+    for (let i = 0; i < nums.length; i++) {        
+        numbers.innerHTML += `<span>${nums[i]}</span>`;
     }
 
-    count--;
-    countDown.innerHTML = count;
-}, 1000);
+    const timer = setInterval(() => {
+
+        let message = '';
+
+        if (count === 0) {
+            clearInterval(timer);
+            message = 'Il tempo è terminato';
+            countDown.innerHTML = message;
+            numbers.classList.add('d-none');
+        } else {
+            count--;
+            countDown.innerHTML = count;
+        }
+
+
+    }, 1000);
+
+};
+
+
+
